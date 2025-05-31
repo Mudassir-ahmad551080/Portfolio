02f86dd5cc34977b5c27d62df4b066b7d4e8bb65
Mudassir Ahmad <mudassir@gemail.com>
2025-05-31 17:08:41 +0500

    commit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -15,13 +15,13 @@ const Portfolio = () => {
                 <span className='underline font-bold'>Feature Project</span>
             </div>
             <div id={theme} className=' w-screen md:ml-15 md:gap-10 flex flex-wrap  items-center'>
-                <div  id={theme} className='bg-yellow-50  border-1 rounded-md  hover:bg-black hover:text-white cursor-pointer shadow-md hover:scale-105 duration-200 h-auto w-72 md:w-78 shadow-md p-5 mt-10 ml-8 md:ml-0'>
+                <div id={theme} className='bg-yellow-50  border-1 rounded-md  hover:bg-black hover:text-white cursor-pointer shadow-md hover:scale-105 duration-200 h-auto w-72 md:w-78 shadow-md p-5 mt-10 ml-8 md:ml-0'>
                     <img className='w-20 h-20 rounded-md' src={mongodb} alt="" />
                     <h1 className='font-bold text-xl font-sans'>MongoDb</h1>
                     <p className='mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia repudiandae asperiores aut explicabo, veniam iste dolorum assumenda libero non praesentium!</p>
                     <div className='flex space-x-10 mt-5'>
-                        <button className='bg-green-400 md:p-3   text-white rounded-md w-[100px] cursor-pointer font-black p-1'>Vedio</button>
-                        <button className='bg-red-500 md:p-3   text-white rounded-md  cursor-pointer font-black p-1'>Source Code</button>
+                        <a href="https://virtual-assistan.netlify.app/" target='_blank' className='bg-green-400 md:p-3 p-1 text-white rounded-md w-[100px] cursor-pointer font-black p-1'>Project</a>
+                       <a target='_blank' href="https://github.com/mudassir-ahmad123/Portfolio.git" className='bg-red-500 md:p-3 p-1 text-white rounded-md  cursor-pointer font-black p-1'>Source Code</a>
                     </div>
                 </div>
                 {/* for React Image */}
@@ -30,8 +30,8 @@ const Portfolio = () => {
                     <h1 className='font-bold text-xl font-sans'>React.Js</h1>
                     <p className='mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia repudiandae asperiores aut explicabo, veniam iste dolorum assumenda libero non praesentium!</p>
                     <div className='flex space-x-10 mt-5'>
-                        <button className='bg-green-400 md:p-3 p-1 text-white rounded-md w-[100px] cursor-pointer font-black p-1'>Vedio</button>
-                        <button className='bg-red-500 md:p-3 p-1 text-white rounded-md  cursor-pointer font-black p-1'>Source Code</button>
+                        <a href="https://tektackteo-app.netlify.app/" target='_blank' className='bg-green-400 md:p-3 p-1 text-white rounded-md w-[100px] cursor-pointer font-black p-1'>Project</a>
+                        <a target='_blank' href="https://github.com/mudassir-ahmad123/virtual-assistant.git" className='bg-red-500 md:p-3 p-1 text-white rounded-md  cursor-pointer font-black p-1'>Source Code</a>
                     </div>
                 </div>
                 {/* for Express Image */}
@@ -40,8 +40,8 @@ const Portfolio = () => {
                     <h1 className='font-bold text-xl font-sans'>Express</h1>
                     <p className='mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia repudiandae asperiores aut explicabo, veniam iste dolorum assumenda libero non praesentium!</p>
                     <div className='flex space-x-10 mt-5'>
-                        <button className='bg-green-400 md:p-3 p-1 text-white rounded-md w-[100px] cursor-pointer font-black '>Vedio</button>
-                        <button className='bg-red-500 md:p-3 p-1 text-white rounded-md  cursor-pointer font-black '>Source Code</button>
+                         <a href="https://virtual-assistan.netlify.app/" target='_blank' className='bg-green-400 md:p-3 p-1 text-white rounded-md w-[100px] cursor-pointer font-black p-1'>Project</a>
+                        <a target='_blank' href="https://github.com/mudassir-ahmad123/tektackteo-app.git" className='bg-red-500 md:p-3 p-1 text-white rounded-md  cursor-pointer font-black p-1'>Source Code</a>
                     </div>
                 </div>
                 {/* for Redux Image */}
@@ -50,7 +50,7 @@ const Portfolio = () => {
                     <h1 className='font-bold text-xl font-sans'>Redux</h1>
                     <p className='mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia repudiandae asperiores aut explicabo, veniam iste dolorum assumenda libero non praesentium!</p>
                     <div className='flex space-x-10 mt-5'>
-                        <button className='bg-green-400 md:p-3 p-1 text-white rounded-md w-[100px] cursor-pointer font-black p-1'>Vedio</button>
+                         <a href="https://virtual-assistan.netlify.app/" target='_blank' className='bg-green-400 md:p-3 p-1 text-white rounded-md w-[100px] cursor-pointer font-black p-1'>Project</a>
                         <button className='bg-red-500 md:p-3 p-1 text-white rounded-md  cursor-pointer font-black '>Source Code</button>
                     </div>
                 </div>
