2cb2ebddf18917b6afcc1d4f436f5aff2c99e180
Mudassir Ahmad <ma6386731@gmail.com>
2025-08-27 22:39:57 +0500

    another commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home'
 import About from './components/About'
 import Portfolio from './components/Portfolio'
 import Exprience from './components/Exprience'
+import gsap from 'gsap'
 import Footer from './components/Footer'
 import Contact from './components/Contact'
 import { useTheme } from './context/ThemeContext'
@@ -12,11 +13,56 @@ import { motion, useScroll } from 'framer-motion'
 import { Toaster } from 'react-hot-toast'
 import ResumeButton from './components/ResumeButton'
 import ContactInfo from './components/ContactInfo'
+import { useGSAP } from '@gsap/react'
 const App = () => {
   const scrollYprogress = useScroll().scrollYProgress
   const [theme] = useTheme();
+ const divref = React.useRef(null);
+   useGSAP(function(){
+     // GSAP animations go here
+     const tl = gsap.timeline();
+     tl.from('.stair',{
+      height: 0,
+      duration: 0.2,
+      stagger: {
+        amount: -0.3
+      }
+     });
+     tl.to('.stair',{
+      y: '100%',
+      stagger:{
+        amount: -0.3
+      },
+     });
+     tl.to(divref.current, {
+      opacity: 0,
+      duration: 0.2,
+      display: 'none'
+     });
+
+   })
   return (
     <>
+    <div ref={divref} className='h-screen w-full leading-tight flex fixed z-500 top-0 '>
+      <div className=' stair h-full w-1/2 bg-zinc-950'>
+
+      </div>
+       <div className=' stair h-full w-1/2 bg-zinc-950'>
+
+      </div>
+      <div className=' stair h-full w-1/2 bg-zinc-950'>
+
+      </div>
+      <div className=' stair h-full w-1/2 bg-zinc-950'>
+
+      </div>
+      <div className=' stair h-full w-1/2 bg-zinc-950'>
+
+      </div>
+      <div className=' stair h-full w-1/2 bg-zinc-950'>
+
+      </div>
+    </div>
       <div id={theme}>
         <motion.div
           style={{
@@ -24,6 +70,7 @@ const App = () => {
           }}
           className='bg-lime-400 h-[2px] mb-20 origin-left   fixed w-full z-120'></motion.div>
         <Navbar />
+
 
         <div className='mt-15 absolute w-full h-[5px] mt-11 fixed mt-1 z-1 hidden md:flex bg-amber-100'></div>
         <Home />
