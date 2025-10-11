87f6218bcbddae25bc3b57fae0664fd8c2dda34b
Mudassir Ahmad <ma6386731@gmail.com>
2025-10-11 10:25:27 +0500

    another commit

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -5,21 +5,21 @@ import { MdOutlineMail } from "react-icons/md";
 const ContactInfo = () => {
   return (
     <div className='flex justify-between flex-col mt-10 mb-10 gap-10 md:gap-0 md:flex-row mx-15'>
-        <div id='namediv' className=' border border-1 p-10'>
-          <div className='flex gap-4'>
+        <div id='namediv' className=' border hover:shadow-[0_0_20px_10px_rgba(0,255,255,0.4)] transition-all duration-500 cursor-pointer border-1 p-10'>
+          <div className='flex   gap-4'>
             <h3 className='font-bold'>Phone</h3>
             <FaPhone className='mt-1'/>
           </div>
           <p className='mt-2'>03104847156</p>
         </div>
-        <div id='namediv' className='border border-1 p-10'>
+        <div id='namediv' className='border hover:shadow-[0_0_20px_10px_rgba(0,255,255,0.4)] transition-all duration-500 cursor-pointer border-1 p-10'>
           <div className='flex gap-4'>
             <h3 className='font-bold'>WhatsApp</h3>
             <FaWhatsapp className='mt-1'/>
           </div>
           <p className='mt-2'>03215837843</p>
         </div>
-        <div id='namediv' className='border border-1 p-10'>
+        <div id='namediv' className='border hover:shadow-[0_0_20px_10px_rgba(0,255,255,0.4)] transition-all duration-500 cursor-pointer border-1 p-10'>
           <div className='flex gap-4'>
             <h3 className='font-bold'>Email</h3>
             <MdOutlineMail className='mt-1'/>
