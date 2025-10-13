cb9e70158576dfae32023b51303696cadbdfeba1
Mudassir Ahmad <ma6386731@gmail.com>
2025-10-13 15:41:00 +0500

    change in the download resume button

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -6,7 +6,7 @@ const ResumeButton = () => {
   return (
    <div className='fixed bottom-1 right-4 z-50'>
      <a  href="/mudassir.pdf" download="Mudassir_Ahmad_Resume.pdf">
-      <button className='resume-button animate-bounce' style={{
+      <button className='resume-button hover:animate-none animate-bounce' style={{
         padding: '10px 20px',
         backgroundColor: '#1f2937',
         color: 'white',
